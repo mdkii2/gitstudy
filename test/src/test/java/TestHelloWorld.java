908a8292ad6aa7e8d8ed598ef5908e7365970588
mdkii2 <sfmdkii@gmail.com>
2017-06-21 20:19:17 +0800

    bug fix

--- test/src/test/java/TestHelloWorld.java
+++ test/src/test/java/TestHelloWorld.java
@@ -3,6 +3,7 @@
  */
 public class TestHelloWorld {
     public static void main(String[] args) {
+        System.out.println("branch merge test");
         System.out.println("Hello World!");
     }
 }
